f2cb663b16325edb9e861945a002d2f4c404d314
Ryota Enoki <ryoenoki2013@gmail.com>
2021-11-09 13:19:54 +0900

    create added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,33 @@
 class TasksController < ApplicationController
+  # /tasks/new
+  def new
+    @task = Task.new
+  end
+  # creating a new task
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path(@task)
+    else
+      render 'new.html.erb'
+    end
+  end
+
   # /tasks
   def index
     @tasks = Task.all
   end
+
+  # /tasks/:id
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
